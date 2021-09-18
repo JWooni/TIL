a9a3f3eb90a1beff667ae29e5488997a3bc5f69d
JWooni <45754698+JWooni@users.noreply.github.com>
2021-09-18 12:50:46 +0900

    mod

--- JavaScript/controlStatement/main.js
+++ JavaScript/controlStatement/main.js
@@ -42,3 +42,52 @@ for(let i = 0; i < 10; i++){
   }
   ulEl.appendChild(li)
 }
+
+// while : 시작과 끝을 모를 때 (최소0번 최대 조건까지 수행)
+const i = 0;
+
+while ( i < 10 ) {
+  // do something
+  i++;
+}
+
+// do ~ while : 시작과 끝을 모를 때 (최소 1번 최대 조건까지 수행)
+const i = 0;
+do {
+  alert(i);
+  i++;
+} while (i < 10)
+
+// 분기문
+// break : switch ~ case, 반복문을 빠져나갈 때
+const i = 0;
+while (i < 10) {
+  if (i > 5) {
+    break;
+  }
+  alert(i);
+  i++;
+}
+
+// continue : 반복문에 수행을 건너뛸 때
+const i = 0;
+while (i < 10) {
+  i++;
+  if (i % 2 === 0) {
+    continue;
+  }
+  alert(i);
+}
+
+// return : 값을 반환하거나 코드이 실행을 멈추고 호출한 곳으로 돌아갈 때
+function text(x) {
+  let i = 0;
+
+  while (i < 6) {
+    if (i == 3) {
+      break;
+    }
+    i += 1;
+  }
+  return i * x;
+}
