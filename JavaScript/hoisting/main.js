51399d263c1f02263b507477a65a0074e22b9038
JWooni <45754698+JWooni@users.noreply.github.com>
2021-09-24 16:25:01 +0900

    add

---
+++ JavaScript/hoisting/main.js
@@ -0,0 +1,13 @@
+// using var
+console.log(name);
+var name = "john";
+console.log(name);
+
+// hoisting
+var name_2;
+console.log(name_2);
+name_2 = "john";
+console.log(name_2);
+
+console.log(name_3);
+let name_3 = "john";
