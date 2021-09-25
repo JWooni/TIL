5e1b600f7bcad78c7deb934402f07c29aa6110cb
JWooni <45754698+JWooni@users.noreply.github.com>
2021-09-25 21:25:35 +0900

    mod

--- JavaScript/operator/main.js
+++ JavaScript/operator/main.js
@@ -54,3 +54,24 @@ if (a){
 }
 
 console.log(a ? '참' : '거짓')
+
+// 비교 연산자
+console.log("Z" > "A"); // true
+console.log("Hello" < "Hi"); // true
+console.log("Hello" >= "Helloo"); // false
+console.log("5" <= 10); // true
+console.log(true == 1); // true
+console.log(false != 123); // true
+console.log(true === 1); // false
+console.log(false !== 123); // true
+
+// 논리 연산자
+console.log(true || false); // true
+console.log(Boolean(0 || false)); // false
+console.log(Boolean(123 || false)); // true
+console.log(Boolean(123 && 0)); // false
+console.log(Boolean(false && true)); // false
+console.log(Boolean(true && 3)); // true
+console.log(Boolean(0 && false)); // false
+console.log(!false); // true
+console.log(!123); // false
